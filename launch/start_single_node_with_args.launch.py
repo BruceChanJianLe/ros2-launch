a5a17d6269fb469e7f39fc74b6569881e15a22a1
chanjl <chanjl@i2r.a-star.edu.sg>
2022-05-08 22:09:02 +0800

    add launch single node with args

---
+++ launch/start_single_node_with_args.launch.py
@@ -0,0 +1,44 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+# Launch Args
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+ARGUMENTS = [
+    DeclareLaunchArgument(
+        'param1',
+        default_value='1',
+        description='The param1 arguement, by default has value 1',
+    ),
+    DeclareLaunchArgument(
+        'param2',
+        default_value='2',
+        description='The param2 arguement, by default has value 2',
+    ),
+]
+
+def generate_launch_description():
+        # Launch args
+        param1 = LaunchConfiguration('param1')
+        param2 = LaunchConfiguration('param2')
+
+        talker_node = Node(
+            package='ros2-launch',
+            executable='ros2_launch_talker',
+            name='ros2_launch_talker_node',
+            parameters=[{
+                'param1' : param1,
+                'param2' : param2,
+            }],
+            # namespace='',
+            # remappings=[
+            #     ('ros2_launch_talker/channel', 'talker')
+            # ]
+        )
+
+        # Define launch description
+        ld = LaunchDescription(ARGUMENTS)
+        ld.add_action(talker_node)
+
+        # Return launch description
+        return ld
