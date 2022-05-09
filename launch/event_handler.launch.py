9f519072506509c570996cab0d0f75bb34b893ac
chanjl <chanjl@i2r.a-star.edu.sg>
2022-05-09 22:45:39 +0800

    add event handler launch file

---
+++ launch/event_handler.launch.py
@@ -0,0 +1,98 @@
+from logging.config import listen
+from sys import stderr, stdout
+from launch import LaunchDescription
+from launch_ros.actions import Node
+# Launch Args
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+# Event Handler
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnExecutionComplete, OnProcessExit, OnProcessIO, OnProcessStart, OnShutdown
+from launch.actions import ExecuteProcess, LogInfo, TimerAction
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import PythonExpression
+
+ARGUMENTS = [
+    DeclareLaunchArgument(
+        'param1',
+        default_value='1',
+        description='The param1 arguement, by default has value 1',
+    ),
+    DeclareLaunchArgument(
+        'param2',
+        default_value='2',
+        description='The param2 arguement, by default has value 2',
+    ),
+]
+
+def generate_launch_description():
+        # Launch args
+        param1 = LaunchConfiguration('param1')
+        param2 = LaunchConfiguration('param2')
+
+        talker_node = Node(
+            package='ros2-launch',
+            executable='ros2_launch_talker',
+            name='ros2_launch_talker_node',
+            parameters=[{
+                'param1' : param1,
+                'param2' : param2,
+            }],
+        )
+
+        listener_node = Node(
+            package='ros2-launch',
+            executable='ros2_launch_listener',
+            name='ros2_launch_listener_node',
+        )
+
+        # On Process Start
+        on_process_start = RegisterEventHandler(
+            OnProcessStart(
+                target_action=talker_node,
+                on_start=[
+                    LogInfo(msg='Talker node has started, launching listener node.'),
+                    listener_node
+                ]
+            )
+        )
+
+        trigger_exec = ExecuteProcess(
+            cmd=[['echo "I am just a trigger"']],
+            shell=True,
+            output={
+                'stderr' : 'screen',
+                'stdout' : 'screen'
+            }
+        )
+
+        # On Process Input Output
+        on_process_io = RegisterEventHandler(
+            OnProcessIO(
+                target_action=trigger_exec,
+                on_stdout=lambda event: LogInfo(msg='Trigger process says: "{}"'.format(event.text.decode().strip()))
+            )
+        )
+
+        # On Process Complete
+        on_process_complete = RegisterEventHandler(
+            OnExecutionComplete(
+                target_action=trigger_exec,
+                on_completion=[
+                    LogInfo(msg='Trigger has been called!'),
+                    TimerAction(period=2.0, actions=[ExecuteProcess(cmd=[['ros2 topic pub /ros2_launch_talker/channel std_msgs/msg/UInt16 "data: 99" --once']], shell=True)]),
+                    # ExecuteProcess(cmd='ros2 topic pub /ros2_launch_talker/channel std_msgs/msg/UInt16 "data: 99" --once', shell=True)
+                ]
+            )
+        )
+
+        # Define launch description
+        ld = LaunchDescription(ARGUMENTS)
+        ld.add_action(talker_node)
+        ld.add_action(on_process_start)
+        ld.add_action(trigger_exec)
+        ld.add_action(on_process_io)
+        ld.add_action(on_process_complete)
+
+        # Return launch description
+        return ld
